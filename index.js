37d9240d75a58fadd7d6e2d5f67fcbad74cd6217
harrisonlu715 <123942925+harrisonlu715@users.noreply.github.com>
2023-07-31 20:47:38 +1000

    1

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 import "./index.css"
+import { profile } from "./data/db.json"
 // import { cards, message, request, profile, highline } from "@/js/database"
 
+import {
+  navbar,
+  navbar_container,
+  navbar_container_logo,
+  navbar_container_searchBar,
+  navbar_container_create,
+  navbar_container_create_img
+} from '@/js/common' //vite.config.js 里面定义 @ --> './src'
+
 /* NAV */
+navbar_container_logo.textContent = "TopSocial"
+navbar_container_create_img.src = profile.img_src
 
 /* LEFT */
 
--- src/js/common.js
+++ src/js/common.js
@@ -1,5 +1,11 @@
 /* NavBar */
 
+export const navbar = document.querySelector("nav")
+export const navbar_container = navbar.querySelector(".container")
+export const navbar_container_logo = navbar_container.querySelector(".logo")
+export const navbar_container_searchBar = navbar_container_logo.querySelector(".search-bar")
+export const navbar_container_create = navbar_container.querySelector(".create")
+export const navbar_container_create_img = navbar_container.querySelector("img")
 
 /* Main */
 
