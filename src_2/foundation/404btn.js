54a0b2e76729047c8d7598425c50c3f1ca57f96c
harrisonlu715 <123942925+harrisonlu715@users.noreply.github.com>
2023-08-13 18:18:03 +1000

    delete scr2

--- src_2/foundation/404btn.js
+++
@@ -1,7 +0,0 @@
-import btn from "../components/button";
-
-export default NotFoundPage = () => {
-  const notFoundPageBtn = document.createElement('div')
-  notFoundPageBtn.appendChild(btn)
-  return notFoundPageBtn
-}
